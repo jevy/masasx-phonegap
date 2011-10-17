ebca5d0c72db5ea9b619083fd02e347d2ce617e8
Jevin Maltais <jevin@quickjack.ca>
2011-10-17 16:09:18 -0400

    Google API works properly.  Need to implement JSONP

--- www/application.coffee
+++ www/application.coffee
@@ -13,6 +13,18 @@ app =
 
 class Entry extends Backbone.Model
 
+class Geolocation extends Backbone.Model
+
+    initialize: ->
+        alert('doing geolocation')
+        address = @get('street') + ', ' + @get('city') + ', ' + @get('province') + ', CA'
+        url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false'
+        url = url.replace(/\s/g,"+");
+        # TODO: Need to implement JSONP pattern to allow for cross site requesting.
+        # Google should be a safe place with no XSS
+        $.getJSON url, (data) ->
+            alert(data)
+
 #
 # Inputing the MASAS secret
 #
@@ -28,7 +40,6 @@ class SecretInputView extends Backbone.View
     @delegateEvents()
 
   next: ->
-    alert('next trigger')
     app.currentEntry.set({secret: $('#entry_secret').val()})
     app.currentView = new SelectGeoView()
 
@@ -48,12 +59,32 @@ class SelectGeoView extends Backbone.View
     @delegateEvents()
 
   manual_geolocate: ->
-    alert('clicked manual button')
-    app.currentEntry.set({use_browser_geolocation: false, street: $('input#street').val(), city: $('input#city').val(), province: $('input#province').val() })
+    location = new Geolocation({street: $('input#street').val(), city: $('input#city').val(), province: $('input#province').val()})
+    app.currentEntry.set({location_of_entry: location}) # Do geocode
     
   auto_geolocate: ->
-    alert('auto locate clicked')
-    app.currentEntry.set({use_browser_geolocation: true})
+    location = request_user_location();
+    #app.currentEntry.set({location_of_entry: new Geolocation({latitude: location.latitude, longitude: location.longitude})}) # Do geocode
+
+  request_user_location: ->
+    return { location: { latitude: 45.8, longitude: -78.3 } }
+
+#
+# Making sure they entered the right location
+#
+
+class ConfirmGeoView extends Backbone.View
+  constructor: ->
+    super
+    @el = $('div#confirm_geo')
+    @delegateEvents()
+
+  render: ->
+    # Pass in Geolocation into a GoogleMapView
+    # Draw it
+
+
+class GoogleMapView extends Backbone.View
 
 #
 # Start the app
--- www/application.js
+++ www/application.js
@@ -1,5 +1,5 @@
 (function() {
-  var Entry, SecretInputView, SelectGeoView, app;
+  var ConfirmGeoView, Entry, Geolocation, GoogleMapView, SecretInputView, SelectGeoView, app;
   var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
@@ -20,6 +20,23 @@
     }
     return Entry;
   })();
+  Geolocation = (function() {
+    __extends(Geolocation, Backbone.Model);
+    function Geolocation() {
+      Geolocation.__super__.constructor.apply(this, arguments);
+    }
+    Geolocation.prototype.initialize = function() {
+      var address, url;
+      alert('doing geolocation');
+      address = this.get('street') + ', ' + this.get('city') + ', ' + this.get('province') + ', CA';
+      url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false';
+      url = url.replace(/\s/g, "+");
+      return $.getJSON(url, function(data) {
+        return alert(data);
+      });
+    };
+    return Geolocation;
+  })();
   SecretInputView = (function() {
     __extends(SecretInputView, Backbone.View);
     SecretInputView.prototype.events = {
@@ -31,7 +48,6 @@
       this.delegateEvents();
     }
     SecretInputView.prototype.next = function() {
-      alert('next trigger');
       app.currentEntry.set({
         secret: $('#entry_secret').val()
       });
@@ -51,21 +67,46 @@
       this.delegateEvents();
     }
     SelectGeoView.prototype.manual_geolocate = function() {
-      alert('clicked manual button');
-      return app.currentEntry.set({
-        use_browser_geolocation: false,
+      var location;
+      location = new Geolocation({
         street: $('input#street').val(),
         city: $('input#city').val(),
         province: $('input#province').val()
       });
-    };
-    SelectGeoView.prototype.auto_geolocate = function() {
-      alert('auto locate clicked');
       return app.currentEntry.set({
-        use_browser_geolocation: true
+        location_of_entry: location
       });
     };
+    SelectGeoView.prototype.auto_geolocate = function() {
+      var location;
+      return location = request_user_location();
+    };
+    SelectGeoView.prototype.request_user_location = function() {
+      return {
+        location: {
+          latitude: 45.8,
+          longitude: -78.3
+        }
+      };
+    };
     return SelectGeoView;
+  })();
+  ConfirmGeoView = (function() {
+    __extends(ConfirmGeoView, Backbone.View);
+    function ConfirmGeoView() {
+      ConfirmGeoView.__super__.constructor.apply(this, arguments);
+      this.el = $('div#confirm_geo');
+      this.delegateEvents();
+    }
+    ConfirmGeoView.prototype.render = function() {};
+    return ConfirmGeoView;
+  })();
+  GoogleMapView = (function() {
+    __extends(GoogleMapView, Backbone.View);
+    function GoogleMapView() {
+      GoogleMapView.__super__.constructor.apply(this, arguments);
+    }
+    return GoogleMapView;
   })();
   $(document).ready(function() {
     app.currentEntry = new Entry;
