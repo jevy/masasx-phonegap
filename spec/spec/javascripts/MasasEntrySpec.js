f3637a25cfa663d7f2eb6f72214180cbf33a2b6f
Jevin Maltais <jevin@quickjack.ca>
2011-11-16 14:23:10 -0500

    Cleaning up MasasEntry and tests

--- spec/spec/javascripts/MasasEntrySpec.js
+++ spec/spec/javascripts/MasasEntrySpec.js
@@ -1,11 +1,63 @@
 describe("MasasEntry", function() {
+  describe(".generate_entry_xml", function() {
     var masas_entry;
 
     beforeEach(function() {
         masas_entry = new MasasEntry();
     });
     
-    it("returns the xml", function() {
-        expect(masas_entry.post_xml()).toEqual("<xml></xml>");
+    it("is bounded by an entry tag", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $title = $xml.find( "entry" );
+      expect($title.attr('xmlns')).toEqual("http://www.w3.org/2005/Atom");
     });
+
+    it("has a status category", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $category = $xml.find( "category[label='Status']" );
+      expect($category.attr('term')).toEqual("Test");
+      expect($category.attr('scheme')).toEqual("http://masas.ca/categories/status");
+    });
+
+    it("has a severity category", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $category = $xml.find( "category[label='Severity']" );
+      expect($category.attr('term')).toEqual("Minor");
+      expect($category.attr('scheme')).toEqual("http://masas.ca/categories/severity");
+    });
+
+    it("has a certainty category", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $category = $xml.find( "category[label='Certainty']" );
+      expect($category.attr('term')).toEqual("Observed");
+      expect($category.attr('scheme')).toEqual("http://masas.ca/categories/certainty");
+    });
+
+    it("has a 'category' category", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $category = $xml.find( "category[label='Category']" );
+      expect($category.attr('term')).toEqual("Other");
+      expect($category.attr('scheme')).toEqual("http://masas.ca/categories/category");
+    });
+
+    it("has an icon", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $category = $xml.find( "category[label='Icon']" );
+      expect($category.attr('term')).toEqual("incident/roadway");
+      expect($category.attr('scheme')).toEqual("http://masas.ca/categories/icon");
+    });
+
+    it("has a title", function() {
+      xmlDoc = $.parseXML( masas_entry.generate_entry_xml() ),
+      $xml = $( xmlDoc ),
+      $title = $xml.find( "title" );
+      expect($title.attr('type')).toEqual("xhtml");
+    });
+  });
 });
--- www/MasasEntry.js
+++ www/MasasEntry.js
@@ -1,6 +1,13 @@
 function MasasEntry() {
 }
 
-MasasEntry.prototype.post_xml = function() {
-    return "<xml></xml>";
+MasasEntry.prototype.generate_entry_xml = function() {
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><entry xmlns=\"http://www.w3.org/2005/Atom\">" +
+      "<category label=\"Status\" scheme=\"http://masas.ca/categories/status\" term=\"Test\" />" +
+      "<category label=\"Severity\" scheme=\"http://masas.ca/categories/severity\" term=\"Minor\" />" +
+      "<category label=\"Certainty\" scheme=\"http://masas.ca/categories/certainty\" term=\"Observed\" />" +
+      "<category label=\"Category\" scheme=\"http://masas.ca/categories/category\" term=\"Other\" />" +
+      "<category label=\"Icon\" scheme=\"http://masas.ca/categories/icon\" term=\"incident/roadway\" />" +
+      "<title type=\"xhtml\"/>" +
+      "</entry>";
 };
